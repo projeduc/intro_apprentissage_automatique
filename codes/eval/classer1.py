ae17dc05e54de38cd73471ba81d6d58cc6bbdac0
kariminf <kariminfo0@gmail.com>
2019-01-05 22:35:41 +0100

    chapitre1: prog class bin2

--- codes/eval/classer1.py
+++ codes/eval/classer1.py
@@ -3,7 +3,10 @@
 # Ici, on va présenter les outils d'évaluation
 # pour le classement binaire
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef
+from sklearn.metrics import accuracy_score, precision_score
+from sklearn.metrics import recall_score, f1_score, matthews_corrcoef
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 # créer une liste de 3 "1" suivis de 17 "0"
 reel = [1] * 3 + [0] * 17
@@ -17,3 +20,14 @@ print "La précision: " + str(precision_score(reel, predit))
 print "Le rappel: " + str(recall_score(reel, predit))
 print "La mesure F1: " + str(f1_score(reel, predit))
 print "corrélation de matthews: " + str(matthews_corrcoef(reel, predit))
+
+# récupérer et afficher la matrice de confusion
+mat_conf = confusion_matrix(reel, predit)
+print mat_conf
+# récupérer le nombre des VN, FP, FN, TP
+vn, fp, fn, vp = mat_conf.ravel()
+print vn, fp, fn, vp
+
+#rapport de classification
+noms_classes = ["desirable", "indesirable"]
+print(classification_report(reel, predit, target_names=noms_classes))
